fec6944cedf8aaa64cbd7e6afd34c7aa433cd256
Emil <emil.nilsen9888@gmail.com>
2021-01-29 13:24:41 +0100

    Disable deno formatting on save

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@
  */
 import { serve } from "https://deno.land/std@0.84.0/http/server.ts";
 const s = serve({ port: 8000 });
-console.log("http://fiwersdoighsdoigesk:w/");
+console.log("http://fihsk:w/");
 for await (const req of s) {
   req.respond({ body: "Hello World\n" });
 }

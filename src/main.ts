77b8ebec6643351902b0ff448a230e25a0b00cfc
Emil <emil.nilsen9888@gmail.com>
2021-01-29 13:47:54 +0100

    Add logging framework

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,11 @@
  * Just some example code to se if the docker application is running
  */
 import { serve } from "https://deno.land/std@0.84.0/http/server.ts";
+import { Logger } from "https://deno.land/x/optic/mod.ts";
+
+const logger = new Logger();
+
+
 const s = serve({ port: 8000 });
 console.log("http://fihsk:w/");
 for await (const req of s) {

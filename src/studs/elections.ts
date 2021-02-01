18e567d5b4472c0ea177ed26f3e9f13d3c41cebf
Steffen Holanger <holanger.steffen@gmail.com>
2021-02-01 18:07:35 +0100

    create dummy datea

--- src/studs/elections.ts
+++ src/studs/elections.ts
@@ -1,1 +1,15 @@
 import { v4 } from "https://deno.land/std/uuid/mod.ts";
+import { Election } from "../models/Election.ts";
+
+const elections: Election[] = [
+  {
+    id: v4.generate(),
+    isCompleted: false,
+  },
+  {
+    id: v4.generate(),
+    isCompleted: false,
+  },
+];
+
+export default elections;

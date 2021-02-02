d71e7088dc9896d43e11ca2a7e1e6be66c763b5f
Sander Hurlen <sander.hurlen@icloud.com>
2021-02-02 16:00:09 +0100

    add models for ballot and election entities

---
+++ src/models/Ballot.ts
@@ -0,0 +1,19 @@
+export enum BallotType {
+  SINGLE,
+  MULTIPLE,
+  RANKED,
+}
+
+export enum BallotResultDisplay {
+  NONE,
+  SINGLE,
+  RUNNER_UP,
+  ALL,
+}
+
+export enum BallotStatus {
+  IN_QUEUE,
+  IN_PROGRESS,
+  IN_RESULT,
+  IN_ARCHIVE,
+}
--- src/models/Election.ts
+++ src/models/Election.ts
@@ -3,21 +3,21 @@ export interface Election {
   electionOrganizer: string;
   title: string;
   description: string;
-  image: string;
-  openDate: Date;
-  closeDate: Date;
+  image?: string;
+  openDate?: Date;
+  closeDate?: Date;
   password: string;
   status: ElectionStatus;
   isLocked: boolean;
   isAutomatic: boolean;
-  created: Date;
-  updated: Date;
+  created?: Date;
+  updated?: Date;
 }
 
 /**
  * Election status represent the state of the election
  */
-enum ElectionStatus {
+export enum ElectionStatus {
   NotStarted,
   Started,
   Finished,

89a18fa571c3d82859ad42b777ffeb3d4aa11ae3
Sander Hurlen <sander.hurlen@icloud.com>
2021-02-02 12:29:56 +0100

    add initial dependencies for typeorm

---
+++ src/deps.ts
@@ -0,0 +1,21 @@
+import { Repository } from "https://denolib.com/denolib/typeorm@v0.2.23-rc10/src/repository/Repository.ts";
+
+export {
+  Column,
+  createConnection,
+  CreateDateColumn,
+  Entity,
+  EntityRepository,
+  getCustomRepository,
+  JoinTable,
+  ManyToMany,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  Repository,
+  UpdateDateColumn,
+  VersionColumn,
+} from "https://denolib.com/denolib/typeorm@v0.2.23-rc10/mod.ts";
+export type {
+  ConnectionOptions,
+} from "https://denolib.com/denolib/typeorm@v0.2.23-rc10/mod.ts";

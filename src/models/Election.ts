fbd78f7edb758f2f2777729dc7aede07f44eb2cd
Steffen Holanger <holanger.steffen@gmail.com>
2021-02-02 14:18:27 +0100

    restructure elections model

--- src/models/Election.ts
+++ src/models/Election.ts
@@ -1,18 +1,31 @@
-export interface Election {
-  id: number;
-  electionOrganizer: string;
-  title: string;
-  description: string;
-  image: string;
-  openDate: Date;
-  closeDate: Date;
-  password: string;
-  status: ElectionStatus;
-  isLocked: boolean;
-  isAutomatic: boolean;
-  created: Date;
-  updated: Date;
-}
+import {
+  array,
+  boolean,
+  DateType,
+  number,
+  Schema,
+  string,
+  Type,
+} from "../deps.ts";
+
+const MIN_PASSWORD_LENGTH = 6;
+
+export type Election = Type<typeof ElectionSchema>;
+// {
+// id: number;
+// electionOrganizer: string;
+// title: string;
+// description: string;
+// image: string;
+// openDate: Date;
+// closeDate: Date;
+// password: string;
+// status: ElectionStatus;
+// isLocked: boolean;
+// isAutomatic: boolean;
+// created: Date;
+// updated: Date;
+// };
 
 /**
  * Election status represent the state of the election
@@ -22,3 +35,19 @@ enum ElectionStatus {
   Started,
   Finished,
 }
+
+export const ElectionSchema = Schema({
+  id: number,
+  electionOrganizer: number,
+  title: string,
+  description: string,
+  image: string.optional(),
+  openDate: DateType,
+  closeDate: DateType,
+  password: string.min(MIN_PASSWORD_LENGTH).optional(),
+  status: ElectionStatus,
+  isLocked: boolean,
+  isAutomatic: boolean,
+});
+
+// export { ElectionSchema };

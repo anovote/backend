74fd34833a291187ee16ac2d74a9b20d5653ee0f
Steffen Holanger <holanger.steffen@gmail.com>
2021-02-02 14:17:26 +0100

    add dependencies links

--- src/deps.ts
+++ src/deps.ts
@@ -1,1 +1,16 @@
+export {
+  array,
+  boolean,
+  DateType,
+  number,
+  string,
+} from "https://denoporter.sirjosh.workers.dev/v1/deno.land/x/computed_types/src/index.ts";
+
+export type {
+  Type,
+} from "https://denoporter.sirjosh.workers.dev/v1/deno.land/x/computed_types/src/index.ts";
+
+import Schema from "https://denoporter.sirjosh.workers.dev/v1/deno.land/x/computed_types/src/index.ts";
+export { Schema };
+
 export { Router } from "https://deno.land/x/oak@v6.5.0/mod.ts";

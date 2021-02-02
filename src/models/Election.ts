9d841b60601b990e0def6a20e6136410c8e8a849
Steffen Holanger <holanger.steffen@gmail.com>
2021-02-02 10:58:17 +0100

    update election interface

--- src/models/Election.ts
+++ src/models/Election.ts
@@ -1,4 +1,24 @@
 export interface Election {
-  id: string;
-  isCompleted: boolean;
+  id: number;
+  electionOrganizer: string;
+  title: string;
+  description: string;
+  image: string;
+  openDate: Date;
+  closeDate: Date;
+  password: string;
+  status: ElectionStatus;
+  isLocked: boolean;
+  isAutomatic: boolean;
+  created: Date;
+  updated: Date;
+}
+
+/**
+ * Election status represent the state of the election
+ */
+enum ElectionStatus {
+  NotStarted,
+  Started,
+  Finished,
 }

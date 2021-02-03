69caf0d3944acd1653477035210b0936b519d589
Steffen Holanger <holanger.steffen@gmail.com>
2021-02-03 13:39:01 +0100

    start fresh making class

--- src/controllers/electionController.ts
+++ src/controllers/electionController.ts
@@ -1,57 +1,79 @@
-import { Election } from "../models/Election.ts";
-import {} from "../deps.ts";
+import { EntityManager } from "https://denolib.com/denolib/typeorm@v0.2.23-rc10/src/entity-manager/EntityManager.ts";
+import { getManager } from "https://denolib.com/denolib/typeorm@v0.2.23-rc10/src/index.ts";
 import { Response } from "https://deno.land/x/oak@v6.5.0/response.ts";
-import { Request } from "https://deno.land/x/oak@v6.5.0/request.ts";
 
-export default {
-  /**
-   * TODO: add comment
-   */
-  getAllElections: getAllElections,
-  createElection: createElection,
-  getElectionById: () => {},
-  updateElectionById: () => {},
-  deleteElectionById: () => {},
-};
+import { Election } from "../models/Election.ts";
 
-function getAllElections(
-  { response }: { response: Response },
-) {
-  await Election.get;
-  response.status = 200;
-  response.body = {
-    success: true,
-    data: elections,
-  };
-}
+export class ElectionController {
+  entityManager: EntityManager = getManager();
 
-async function createElection(
-  { request, response }: {
-    request: Request;
-    response: Response;
-  },
-) {
-  console.log("Create user");
+  async getAllElections({ response }: { response: Response }) {
+    const elections: Election[] = await this.entityManager.getRepository(
+      Election,
+    ).find();
 
-  if (!request.hasBody) {
-    response.status = 400;
     response.body = {
-      success: false,
-      msg: "No data",
+      success: true,
+      data: elections,
     };
-    return;
+    response.status = 200;
   }
-
-  const electionData = await request.body().value as Election;
-  console.log(electionData);
-
-  const [error, election] = validator(electionData);
-  console.log(election);
-
-  console.log(error?.message);
-
-  // validateElectionData(electionData);
 }
 
-function getElectionById({ response }: { response: any }) {
-}
+// import { Election } from "../models/Election.ts";
+// import {} from "../deps.ts";
+// import { Response } from "https://deno.land/x/oak@v6.5.0/response.ts";
+// import { Request } from "https://deno.land/x/oak@v6.5.0/request.ts";
+
+// export default {
+//   /**
+//    * TODO: add comment
+//    */
+//   getAllElections: getAllElections,
+//   createElection: createElection,
+//   getElectionById: () => {},
+//   updateElectionById: () => {},
+//   deleteElectionById: () => {},
+// };
+
+// function getAllElections(
+//   { response }: { response: Response },
+// ) {
+//   await Election.get;
+//   response.status = 200;
+//   response.body = {
+//     success: true,
+//     data: elections,
+//   };
+// }
+
+// async function createElection(
+//   { request, response }: {
+//     request: Request;
+//     response: Response;
+//   },
+// ) {
+//   console.log("Create user");
+
+//   if (!request.hasBody) {
+//     response.status = 400;
+//     response.body = {
+//       success: false,
+//       msg: "No data",
+//     };
+//     return;
+//   }
+
+//   const electionData = await request.body().value as Election;
+//   console.log(electionData);
+
+//   const [error, election] = validator(electionData);
+//   console.log(election);
+
+//   console.log(error?.message);
+
+//   // validateElectionData(electionData);
+// }
+
+// function getElectionById({ response }: { response: any }) {
+// }

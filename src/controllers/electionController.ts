9847c2ab9989df6e2074359c670c4860453a8582
Steffen Holanger <holanger.steffen@gmail.com>
2021-02-03 09:21:59 +0100

    wip

--- src/controllers/electionController.ts
+++ src/controllers/electionController.ts
@@ -1,3 +1,4 @@
+import { Election, ElectionSchema } from "../models/Election.ts";
 import elections from "../studs/elections.ts";
 
 export default {
@@ -11,6 +12,11 @@ export default {
   deleteElectionById: () => {},
 };
 
+const validator = ElectionSchema.destruct();
+console.log(ElectionSchema);
+
+console.log(validator);
+
 function getAllElections(
   { response }: { response: any },
 ) {
@@ -21,9 +27,11 @@ function getAllElections(
   };
 }
 
-function createElection(
+async function createElection(
   { request, response }: { request: any; response: any },
 ) {
+  console.log("Create user");
+
   if (!request.hasBody) {
     response.status = 400;
     response.body = {
@@ -33,9 +41,48 @@ function createElection(
     return;
   }
 
-function validatePassword(password: string): boolean {
-  if (password.length < 6) {
-    throw new Error("password is to short");
-  }
-  return true;
+  const electionData = await request.body().value as Election;
+  console.log(electionData);
+
+  const [error, election] = validator(electionData);
+  console.log(election);
+
+  console.log(error?.message);
+
+  // validateElectionData(electionData);
 }
+
+function getElectionById({ response }: { response: any }) {
+}
+
+// /**
+//  * Validates election data
+//  * @param data the data to be validated
+//  * @returns true if election data is valid, else false
+//  */
+// function validateElectionData(data: Election): boolean {
+//   const { title, description, password, openDate, closeDate } = data;
+//   if (!title || !description) {
+//     throw new Error("Title or description missing");
+//   }
+//   if (password) {
+//     validatePassword(password);
+//   }
+
+//   validateOpenCloseDate(openDate, closeDate);
+
+//   return true;
+// }
+
+// function validatePassword(password: string): void {
+//   if (password.length < 6) {
+//     throw new Error("password is to short");
+//   }
+// }
+
+// function validateOpenCloseDate(open: Date, close: Date) {
+//   if (open > close) {
+//     throw new Error("Open date is after close date");
+//   }
+//   return true;
+// }

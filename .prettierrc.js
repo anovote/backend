4ca70fe2ad083911e48cab0a88c96ba9ab85796f
Christoffer A Traeen <christoffer.andersen@live.com>
2021-02-04 23:02:51 +0100

    Fix format and lint

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,7 @@
-// Denp style
 module.exports = {
-  semi: true,
-  trailingComma: "all",
-  singleQuote: false,
+  semi: false,
+  trailingComma: 'none',
+  singleQuote: true,
   printWidth: 120,
-  tabWidth: 2,
-};
+  tabWidth: 2
+}

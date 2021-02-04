b81e55100f642d60be1f2431c02168a2ddd1e810
Steffen Holanger <holanger.steffen@gmail.com>
2021-02-04 15:16:14 +0100

    fix typo

--- src/models/Election.ts
+++ src/models/Election.ts
@@ -25,7 +25,7 @@ export enum ElectionStatus {
 /**
  * An entity for storing an election. 
  * An election is instantiated by an election organizer. The election organizer can create many elections. 
- * An election hols information about the status, info and other information about the election. If the election requires to add eligble voters, 
+ * An election hols information about the status, info and other information about the election. If the election requires to add eligible voters, 
  * an election can have many eligible voters. 
  * The purpose of an election entity is to hold ballots, which an election can have many of. 
  */

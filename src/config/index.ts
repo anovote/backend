bf730765c0b3ce73b320677226c6c9debbb73e36
Christoffer A Traeen <christoffer.andersen@live.com>
2021-02-04 23:48:51 +0100

    Fix format

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,14 +3,14 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export default {
-    database: {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        db: process.env.POSTGRES_DB,
-        user: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PASSWORD
-    },
-    http:{
-        port: process.env.HTTP_PORT
-    }
+  database: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    db: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD
+  },
+  http: {
+    port: process.env.HTTP_PORT
+  }
 }

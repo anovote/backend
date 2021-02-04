8a5cc77d171b01049b760e7c1ff50cb12dffbb04
Steffen Holanger <holanger.steffen@gmail.com>
2021-02-04 13:46:07 +0100

    fix routes with context

--- src/routes/electionsRoutes.ts
+++ src/routes/electionsRoutes.ts
@@ -8,12 +8,18 @@ const router = new Router();
 const electionController = new ElectionController();
 
 const ELECTIONS_STRING = "/elections";
-const ELECTIONS_ID_STRING = `"${ELECTIONS_STRING}/:id"`;
+const ELECTIONS_ID_STRING = `${ELECTIONS_STRING}/:id`;
 router
-  .get(ELECTIONS_STRING, electionController.getAllElections);
-// .post(ELECTIONS_STRING, electionController.createElection)
-// .get(ELECTIONS_ID_STRING, electionController.getElectionById)
-// .post(ELECTIONS_ID_STRING, electionController.updateElectionById)
-// .delete(ELECTIONS_ID_STRING, electionController.deleteElectionById);
+  .get(ELECTIONS_STRING, electionController.getAllElections)
+  .post(ELECTIONS_STRING, (ctx) => electionController.createElection(ctx))
+  // .get(ELECTIONS_ID_STRING, electionController.getElectionById)
+  .put(
+    ELECTIONS_ID_STRING,
+    (ctx) => electionController.updateElectionById(ctx),
+  )
+  .delete(
+    ELECTIONS_ID_STRING,
+    (ctx) => electionController.deleteElectionById(ctx),
+  );
 
 export default router;

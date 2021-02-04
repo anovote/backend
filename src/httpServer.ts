b41ac8157dc0fe49ee00d0da94e8c2614f29500f
Sander Hurlen <sander.hurlen@icloud.com>
2021-02-04 18:56:49 +0100

    add auth router and initiate db connection

--- src/httpServer.ts
+++ src/httpServer.ts
@@ -1,11 +1,31 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 import { green, yellow } from "https://deno.land/std@0.85.0/fmt/colors.ts";
 import electionRouter from "./routes/elections.ts";
+import authRouter from "./routes/auth.ts";
+import { config, ConnectionOptions, createConnection } from "./deps.ts";
 
 const PORT = 8000;
 
-const router = new Router();
+const dbConfig: ConnectionOptions = {
+  type: "postgres",
+  host: config.get("DB_HOST"),
+  port: Number.parseInt(config.get("DB_PORT")!),
+  username: config.get("POSTGRES_USER"),
+  password: config.get("POSTGRES_PASSWORD"),
+  database: config.get("POSTGRES_DB"),
+  entities: [
+    Deno.cwd() + "/src/entity/barrel.ts",
+  ],
+  synchronize: true,
+};
 
+try {
+  const connection = await createConnection(dbConfig);
+} catch (e) {
+  console.error(e);
+}
+
+const router = new Router();
 /**
  * These are all example routes and not meant to be
  * part of a solution. For a final solution the routes
@@ -25,6 +45,7 @@ router
 const app = new Application();
 app.use(router.routes());
 app.use(router.allowedMethods());
+app.use(authRouter.routes());
 app.use(electionRouter.routes());
 app.use(electionRouter.allowedMethods());
 

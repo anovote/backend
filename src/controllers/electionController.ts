3f78aec56aa5d04f1bb775cd52f3940464540350
Steffen Holanger <holanger.steffen@gmail.com>
2021-02-04 09:37:00 +0100

    update getAll and create methods

--- src/controllers/electionController.ts
+++ src/controllers/electionController.ts
@@ -1,19 +1,26 @@
-// import { EntityManager } from "https://denolib.com/denolib/typeorm@v0.2.23-rc10/src/entity-manager/EntityManager.ts";
+import { EntityManager } from "https://denolib.com/denolib/typeorm@v0.2.23-rc10/src/entity-manager/EntityManager.ts";
 import {
-  // getManager,
+  getManager,
   getRepository,
 } from "https://denolib.com/denolib/typeorm@v0.2.23-rc10/src/index.ts";
 import { Response } from "https://deno.land/x/oak@v6.5.0/response.ts";
+import { Request } from "https://deno.land/x/oak@v6.5.0/request.ts";
 // import { getRepository } from "https://denolib.com/denolib/typeorm@v0.2.23-rc10/src/index.ts";
 
 import { Election } from "../models/Election.ts";
+import { titleCase } from "https://denolib.com/denolib/typeorm@v0.2.23-rc10/src/util/StringUtils.ts";
+import { optional } from "https://denoporter.sirjosh.workers.dev/v1/deno.land/x/computed_types/src/schema/logic.ts";
 
 export default class ElectionController {
-  // entityManager: EntityManager = getManager();
+  // entityManager: EntityManager;
+
+  // constructor() {
+  //   this.entityManager = ;
+  // }
 
   async getAllElections({ response }: { response: Response }) {
     console.log("get to function");
-
+    // this.entityManager = getManager();
     try {
       const elections: Election[] | undefined =
         // await undefined;
@@ -30,6 +37,38 @@ export default class ElectionController {
     } catch (error) {
       console.error(error);
     }
+  }
+
+  async createElection(
+    { request, response }: { request: Request; response: Response },
+  ) {
+    // this.entityManager = getManager();
+
+    if (!request.hasBody) {
+      response.status = 400;
+      response.body = {
+        success: false,
+        errorMessage: "No data",
+      };
+      return;
+    }
+
+    const requestBody = request.body({ type: "json" });
+
+    const values = await requestBody.value;
+    const { title, description } = values;
+
+    const election: Election = new Election();
+    election.title = title;
+    election.description = description;
+    console.log(election);
+
+    await getRepository(Election).save(election);
+    response.status = 200;
+    response.body = {
+      succsess: true,
+      data: election,
+    };
   }
 }
 

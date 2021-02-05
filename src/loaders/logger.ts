d5956e54c69ad871a25d2e39731d85c0e9864f85
Christoffer A Traeen <christoffer.andersen@live.com>
2021-02-05 01:38:12 +0100

    Add winston

---
+++ src/loaders/logger.ts
@@ -0,0 +1,34 @@
+import winston from 'winston'
+/**
+ * Setsup Winston
+ *
+ * https://github.com/winstonjs/winston
+ */
+
+// Date format used in messages
+const dateFormat = 'DD-MM-YYYY - HH:mm:ss'
+
+// Transports for where to log
+const transports = []
+
+// Creates new transports
+transports.push(
+  new winston.transports.Console({
+    format: winston.format.combine(winston.format.cli(), winston.format.splat())
+  })
+)
+
+// Initialize the logger instance
+const logger = winston.createLogger({
+  format: winston.format.combine(
+    winston.format.timestamp({
+      format: dateFormat
+    }),
+    winston.format.errors({ stack: true }),
+    winston.format.splat(),
+    winston.format.json()
+  ),
+  transports
+})
+
+export { logger }

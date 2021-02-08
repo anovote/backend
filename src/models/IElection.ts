35cf64a350ee17ef0babf4f98be48d0470a12d2a
Steffen Holanger <holanger.steffen@gmail.com>
2021-02-08 10:50:32 +0100

    rename file

---
+++ src/models/IElection.ts
@@ -0,0 +1,24 @@
+export interface Election {
+  id: number
+  electionOrganizer: string
+  title: string
+  description: string
+  image?: string
+  openDate?: Date
+  closeDate?: Date
+  password: string
+  status: ElectionStatus
+  isLocked: boolean
+  isAutomatic: boolean
+  created?: Date
+  updated?: Date
+}
+
+/**
+ * Election status represent the state of the election
+ */
+export enum ElectionStatus {
+  NotStarted,
+  Started,
+  Finished
+}

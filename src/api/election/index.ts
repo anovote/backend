51880e3b5b65523aae00d3be4d6d2e505690cf3c
Steffen Holanger <holanger.steffen@gmail.com>
2021-02-08 13:46:09 +0100

    fix router returns

--- src/api/election/index.ts
+++ src/api/election/index.ts
@@ -1,17 +1,26 @@
 import { database } from '@/loaders'
+import { Election } from '@/models/election/Election'
 import { ElectionService } from '@/services/ElectionService'
 import { Router } from 'express'
+import { StatusCodes } from 'http-status-codes'
 
 const router = Router()
 
-router.post('/', (request, response) => {
+router.post('/', async (request, response) => {
   try {
     const electionService = new ElectionService(database)
-    electionService.createElection(request.body)
-    response.send('Created :D')
+    const election: Election | undefined = await electionService.createElection(request.body)
+    response.status(StatusCodes.CREATED).json(election)
   } catch (error) {
     response.status(400).send()
   }
+})
+
+router.get('/', async (request, response) => {
+  const electionService = new ElectionService(database)
+  const elections: Election[] | undefined = await electionService.getAllElections()
+  response.status(200)
+  response.json(elections)
 })
 
 router.get('/:id', (request, response) => {

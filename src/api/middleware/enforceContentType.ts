e9f3e4f73b40f9d049f6f7d4a09c0355feaae12c
Steffen Holanger <holanger.steffen@gmail.com>
2021-02-09 13:56:37 +0100

    rename file

--- src/api/middleware/enforceContentType.ts
+++
@@ -1,7 +0,0 @@
-import * as bodyParser from 'body-parser'
-import { NextFunction } from 'connect'
-
-export function enforceContentType(next: NextFunction) {
-  bodyParser.json({ type: 'application/json' })
-  next()
-}

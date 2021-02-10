9ea86040acd3f4be4fcdfc6fd87efdca4f0ed34c
Christoffer A Traeen <christoffer.andersen@live.com>
2021-02-10 11:16:54 +0100

    Add dummy election generator for tests

---
+++ tests/helpers/seed/election.ts
@@ -0,0 +1,32 @@
+import { Election } from '@/models/Election'
+import { ElectionStatus } from '@/models/Election/enums'
+import { ElectionOrganizer } from '@/models/ElectionOrganizer'
+import { EncryptionService } from '@/services/EncryptionService'
+import { Connection } from 'typeorm'
+
+export const createDummyElection = async (connection: Connection, organizer: ElectionOrganizer) => {
+  const repository = connection.getRepository(Election)
+  const encryptionService = new EncryptionService()
+
+  const election = repository.create({
+    title: 'Election 2000',
+    password: await encryptionService.hash('123456'),
+    status: ElectionStatus.NotStarted,
+    electionOrganizer: organizer,
+    description: 'Some random description',
+    image: 'img.png',
+    openDate: new Date(),
+    closeDate: new Date(),
+    isLocked: true,
+    isAutomatic: false,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    eligibleVoters: []
+  })
+  return repository.save(election)
+}
+
+export const deleteDummyElections = async (connection: Connection, elections: Array<Election>) => {
+  const repository = connection.getRepository(Election)
+  repository.remove(elections)
+}

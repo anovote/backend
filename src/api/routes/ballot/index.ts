5de9973ad7d103cd7a1befa9ef7a3cfd1afecb7d
Christoffer A Traeen <christoffer.andersen@live.com>
2021-02-11 23:46:05 +0100

    Crud ballot

--- src/api/routes/ballot/index.ts
+++ src/api/routes/ballot/index.ts
@@ -1,24 +1,61 @@
 import { database } from '@/loaders'
+import { logger } from '@/loaders/logger'
 import { Ballot } from '@/models/Ballot/BallotEntity'
+import { IBallot } from '@/models/Ballot/IBallot'
+import { BallotService } from '@/services/BallotService'
 import { Router } from 'express'
+import { StatusCodes } from 'http-status-codes'
 
 const router = Router()
 
 router.post('/', async (request, response) => {
   try {
-    const db = database.getRepository(Ballot)
-    await db.save(db.create(request.body))
-    return response.send('Created :D')
+    const ballotSerivce = new BallotService(database)
+    const newBallot = request.body as IBallot
+    const ballot = await ballotSerivce.create(newBallot)
+    return ballot ? response.send(ballot) : response.status(StatusCodes.BAD_REQUEST).send('Could not create the ballot')
   } catch (error) {
-    console.log(error)
-
-    response.status(400).send()
+    return response.status(StatusCodes.INTERNAL_SERVER_ERROR).send()
   }
 })
 
-router.get('/:id', (request, response) => {
-  console.log('GET')
-  response.send()
+router.get('/:id', async (request, response) => {
+  try {
+    // TODO Validate that the user owns/ is allowd to get this ballot
+    const ballotSerivce = new BallotService(database)
+    const id = Number.parseInt(request.params.id)
+    const ballot = await ballotSerivce.get(id)
+    return ballot instanceof Ballot ? response.send(ballot) : response.status(StatusCodes.NOT_FOUND).send()
+  } catch (error) {
+    response.status(StatusCodes.INTERNAL_SERVER_ERROR).send()
+  }
+})
+
+router.delete('/:id', async (request, response) => {
+  try {
+    // TODO Validate that the user owns/ is allowd to delete this ballot
+    const ballotSerivce = new BallotService(database)
+    const id = Number.parseInt(request.params.id)
+    const deletedBallot = await ballotSerivce.delete(id)
+    deletedBallot ? response.status(StatusCodes.OK) : response.status(StatusCodes.NOT_FOUND)
+    response.send()
+  } catch (error) {
+    logger.error(error)
+    return response.status(StatusCodes.BAD_REQUEST).send('Something went wrong')
+  }
+})
+
+router.put('/:id', async (request, response) => {
+  try {
+    // TODO Validate that the user owns/ is allowd to update this ballot
+    const ballotSerivce = new BallotService(database)
+    const id = Number.parseInt(request.params.id)
+    const ballot = request.body as IBallot
+    const updatedBallot = await ballotSerivce.update(id, ballot)
+    updatedBallot ? response.send(updatedBallot) : response.status(StatusCodes.NOT_FOUND).send()
+  } catch (error) {
+    response.status(StatusCodes.INTERNAL_SERVER_ERROR).send()
+  }
 })
 
 export default router
--- src/services/BallotService.ts
+++ src/services/BallotService.ts
@@ -1,41 +1,93 @@
 import { logger } from '@/loaders/logger'
 import { Ballot } from '@/models/Ballot/BallotEntity'
 import { IBallot } from '@/models/Ballot/IBallot'
-import { Connection, QueryFailedError, Repository } from 'typeorm'
+import { Connection, Repository } from 'typeorm'
+import { ElectionService } from './ElectionService'
 
+/**
+ * Responsible for providng services for handling ballots.
+ * It provides methods for creating, updating, deleting and getting ballot(s)
+ */
 export class BallotService {
   private _database: Connection
   private _ballotRepository: Repository<Ballot>
+  private _electionService?: ElectionService
 
-  constructor(database: Connection) {
+  constructor(database: Connection, electionService?: ElectionService) {
     this._database = database
     this._ballotRepository = this._database.getRepository(Ballot)
+    this._electionService = electionService
   }
 
+  /**
+   * Creates a new ballot and return the created ballot entity.
+   * Throws error if database, or query fails
+   * @param newBallot the ballot to create
+   */
   async create(newBallot: IBallot) {
     try {
-      
-      const ballotEntiy = this._ballotRepository.create(newBallot)
-      await this._ballotRepository.save(ballotEntiy)
-    } catch (error) {
-      if (error instanceof QueryFailedError) {
-        logger.error(error)
+      if (this._electionService) {
+        const election = await this._electionService.getElectionById(newBallot.election)
+        if (!election) return
+        const ballotEntiy = this._ballotRepository.create(newBallot)
+        ballotEntiy.id = -1
+        return await this._ballotRepository.save(ballotEntiy)
       }
+      return
+    } catch (error) {
+      logger.error(error)
+      throw new Error('Unable to create ballot')
     }
   }
 
-  async update() {
+  /**
+   * Updates the provided ballot with the given id. If it is found and updated, it returns
+   * the updated entity. Else it return undefined.
+   * Throws error if database, or query fails
+   * @param id the id of the ballot to update
+   * @param updatedBallot the updated ballot details
+   */
+  async update(id: number, updatedBallot: IBallot) {
     try {
-    } catch (error) {}
+      const existingBallot = await this._ballotRepository.findOne({ id })
+      if (!existingBallot) return
+      // Todo; make better implementation of this (makes sure it cant be updated by user).
+      delete (updatedBallot as any)['createdAt']
+      delete (updatedBallot as any)['updatedAt']
+      return await this._ballotRepository.update(id, updatedBallot)
+    } catch (error) {
+      logger.error(error)
+      throw new Error('Failed to update ballot')
+    }
   }
-
-  async delete() {
+  /**
+   * Deletes a ballot by the given ID, if it exists,
+   * the ballot is returned, else undefined.
+   * Throws error if database, or query fails
+   * @param id the id of the ballot to delete
+   */
+  async delete(id: number) {
     try {
-    } catch (error) {}
+      const existingBallot = await this._ballotRepository.findOne(id)
+      if (!existingBallot) return
+      return await this._ballotRepository.remove(existingBallot)
+    } catch (error) {
+      logger.error(error)
+      throw new Error('Failed to delete ballot')
+    }
   }
-
-  async get() {
+  /**
+   * Tries to get a ballot with the given ID, if the ballot exists return it,
+   * else return undefined.
+   * Throws error if database, or query fails
+   * @param id the id of the ballot to get
+   */
+  async get(id: number) {
     try {
-    } catch (error) {}
+      return await this._ballotRepository.findOne({ id })
+    } catch (error) {
+      logger.error(error)
+      throw new Error('Failed to get ballot')
+    }
   }
 }

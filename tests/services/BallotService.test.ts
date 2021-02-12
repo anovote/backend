bd59d930665a26dbef95ac3ac0471206e8a3a464
Christoffer A Traeen <christoffer.andersen@live.com>
2021-02-12 02:04:14 +0100

    Add tests

--- tests/services/BallotService.test.ts
+++ tests/services/BallotService.test.ts
@@ -1,5 +1,11 @@
+import { NotFoundError } from '@/lib/Errors/NotFoundError'
+import { Ballot } from '@/models/Ballot/BallotEntity'
+import { BallotResultDisplay } from '@/models/Ballot/BallotResultDisplay'
+import { BallotType } from '@/models/Ballot/BallotType'
 import { Election } from '@/models/Election/ElectionEntity'
 import { ElectionOrganizer } from '@/models/ElectionOrganizer/ElectionOrganizerEntity'
+import { BallotService } from '@/services/BallotService'
+import { ElectionService } from '@/services/ElectionService'
 import { Connection } from 'typeorm'
 import { getTestDatabase } from '../helpers/database'
 import { createDummyElection, deleteDummyElections } from '../helpers/seed/election'
@@ -8,34 +14,135 @@ import { createDummyOganizer, deleteDummyOrganizer } from '../helpers/seed/organ
 let db: Connection
 let organizer: ElectionOrganizer
 let election: Election
-
+let ballotSerivce: BallotService
+let ballots: Ballot[] = []
+let seedBallot: Ballot
 beforeAll(async () => {
   db = await getTestDatabase()
   organizer = await createDummyOganizer(db)
   election = await createDummyElection(db, organizer)
+  ballotSerivce = new BallotService(db, new ElectionService(db))
+
+  seedBallot = await (ballotSerivce.create({
+    candidates: [],
+    election: election.id,
+    order: 1,
+    displayResultCount: true,
+    resultDisplayType: BallotResultDisplay.ALL,
+    resultDisplayTypeCount: 2,
+    title: 'Test ballot',
+    type: BallotType.MULTIPLE,
+    description: 'Description',
+    image: 'img.png'
+  }) as Promise<Ballot>)!
 })
 
 afterAll(async () => {
+  for (const ballot of ballots) {
+    await ballotSerivce.delete(ballot.id)
+  }
+  await ballotSerivce.delete(seedBallot.id)
   await deleteDummyElections(db, [election])
   await deleteDummyOrganizer(db, organizer)
 })
 
-it('should create a ballot', () => {
-  expect(false).toBe(true)
+it('should create a ballot with all data filled out', async () => {
+  const ballot = await ballotSerivce.create({
+    candidates: [],
+    election: election.id,
+    order: 1,
+    displayResultCount: true,
+    resultDisplayType: BallotResultDisplay.ALL,
+    resultDisplayTypeCount: 2,
+    title: 'Test ballot',
+    type: BallotType.MULTIPLE,
+    description: 'Description',
+    image: 'img.png'
+  })
+  expect(ballot).toBeInstanceOf(Ballot)
+  ballots.push(ballot!)
 })
 
-it('should not create a ballot if election does not exist', () => {
-  expect(false).toBe(true)
+it('should create a ballot without image and description', async () => {
+  const ballot = await ballotSerivce.create({
+    candidates: [],
+    election: election.id,
+    order: 1,
+    displayResultCount: true,
+    resultDisplayType: BallotResultDisplay.ALL,
+    resultDisplayTypeCount: 2,
+    title: 'Test ballot',
+    type: BallotType.MULTIPLE
+  })
+  expect(ballot).toBeInstanceOf(Ballot)
+  ballots.push(ballot!)
 })
 
-it('should not create a ballot', () => {
-  expect(false).toBe(true)
+it('should not create a ballot if election does not exist with not found excpetion', async () => {
+  try {
+    const ballot = await ballotSerivce.create({
+      candidates: [],
+      election: 9999999,
+      order: 1,
+      displayResultCount: true,
+      resultDisplayType: BallotResultDisplay.ALL,
+      resultDisplayTypeCount: 2,
+      title: 'Test ballot',
+      type: BallotType.MULTIPLE
+    })
+  } catch (error) {
+    expect(error).toBeInstanceOf(NotFoundError)
+  }
 })
 
-it('should return the ballot with given id', () => {
-  expect(false).toBe(true)
+it('should not create a ballot', async () => {
+  try {
+    const ballot = await ballotSerivce.create({
+      candidates: [],
+      election: election.id,
+      order: 1,
+      displayResultCount: true,
+      resultDisplayType: 99,
+      resultDisplayTypeCount: 2,
+      title: 'Test ballot',
+      type: 77
+    })
+  } catch (error) {
+    expect(error).toBeInstanceOf(Error)
+  }
 })
 
-it('should return undefined if ballot does not exist', () => {
-  expect(false).toBe(true)
+it('should return the ballot with given id', async () => {
+  const ballot = await ballotSerivce.get(seedBallot.id)
+  expect(ballot!.id).toBe(seedBallot.id)
+})
+
+it('should return undefined if ballot does not exist', async () => {
+  const ballot = await ballotSerivce.get(999999)
+  expect(ballot).toBeUndefined()
+})
+
+it('should delete a ballot which exists', async () => {
+  const ballot = await ballotSerivce.create({
+    candidates: [],
+    election: election.id,
+    order: 1,
+    displayResultCount: true,
+    resultDisplayType: BallotResultDisplay.ALL,
+    resultDisplayTypeCount: 2,
+    title: 'Test ballot',
+    type: BallotType.MULTIPLE,
+    description: 'Description',
+    image: 'img.png'
+  })
+  const deleted = await ballotSerivce.delete(ballot!.id)
+  expect(deleted!.title).toBe(ballot!.title)
+})
+
+it('should throw not found eror when deleting a ballot which do not exist', async () => {
+  try {
+    const deleted = await ballotSerivce.delete(99999999)
+  } catch (error) {
+    expect(error).toBeInstanceOf(NotFoundError)
+  }
 })

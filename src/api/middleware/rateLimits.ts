01711ce3682ebea2f6885aa12d59bd2b92b8612a
Steffen Holanger <holanger.steffen@gmail.com>
2021-02-17 12:08:12 +0100

    rename file

--- src/api/middleware/rateLimits.ts
+++ src/api/middleware/rateLimits.ts
@@ -1,14 +1,19 @@
 import rateLimit from 'express-rate-limit'
 
-export const apiLimiter = rateLimit({
+const apiLimiter = rateLimit({
   windowMs: 10 * 60 * 1000,
   max: 100,
   message: 'Request limit reached',
   headers: true
 })
-export const loginLimiter = rateLimit({
+const loginLimiter = rateLimit({
   windowMs: 10 * 60 * 1000,
   max: 10,
   message: 'Too many login attempts. Try again later',
   headers: true
 })
+
+export const rateLimits = {
+  apiLimiter,
+  loginLimiter
+}

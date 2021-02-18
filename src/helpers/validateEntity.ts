9350a62592d5006a5c722f057729ef2f5c601f3b
Sander Hurlen <sander.hurlen@icloud.com>
2021-02-18 08:39:00 +0100

    add validate helper method

---
+++ src/helpers/validateEntity.ts
@@ -0,0 +1,15 @@
+import { validate } from 'class-validator'
+import { ValidationError } from '@/lib/errors/validation/ValidationError'
+/**
+ * Validates an entity according to its entity contraints or throws an validation error
+ * @param entity a entity to validate on
+ */
+export const validateEntity = async (entity: object) => {
+  const validation = await validate(entity)
+  const isValid = validation.length === 0
+
+  if (!isValid) {
+    const name = validation[0].target?.constructor.name!
+    throw new ValidationError({ message: `Validation failed for ${name}`, validation: validation })
+  }
+}

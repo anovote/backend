2283c22c053365dc83c7d018bebedffe04aacf41
Sander Hurlen <sander.hurlen@icloud.com>
2021-02-19 15:43:56 +0100

    refactor election org to inject database

--- src/api/routes/auth/index.ts
+++ src/api/routes/auth/index.ts
@@ -3,14 +3,16 @@ import { Router } from 'express'
 import { ElectionOrganizerService } from '@/services/ElectionOrganizerService'
 import { StatusCodes } from 'http-status-codes'
 import { BadRequestError } from '@/lib/errors/http/BadRequestError'
+import { database } from '@/loaders'
 
 const authService = new AuthenticationService()
-const electionOrganizerService = new ElectionOrganizerService()
 const router = Router()
 
 router.post('/register', async (request, response, next) => {
+  const electionOrganizerService = new ElectionOrganizerService(database)
   try {
-    const token = await electionOrganizerService.createAndSaveElectionOrganizer(request.body)
+    const organizer = await electionOrganizerService.createAndSaveElectionOrganizer(request.body)
+    const token = await authService.generateTokenFromId(organizer.id)
     response.status(StatusCodes.CREATED).json({ token })
   } catch (error) {
     next(error)

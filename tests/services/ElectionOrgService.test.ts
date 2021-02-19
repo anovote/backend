b9c07f1ab050c361060365d714e0a650f53089a3
Sander Hurlen <sander.hurlen@icloud.com>
2021-02-19 15:43:31 +0100

    refactor election org service to use database injection and add tests

---
+++ tests/services/ElectionOrgService.test.ts
@@ -0,0 +1,43 @@
+import { Election } from '@/models/Election/ElectionEntity'
+import { ElectionOrganizer } from '@/models/ElectionOrganizer/ElectionOrganizerEntity'
+import { ElectionOrganizerService } from '@/services/ElectionOrganizerService'
+import { Connection } from 'typeorm'
+import { getTestDatabase } from '../helpers/database'
+
+describe('election organizer service', () => {
+  let db: Connection
+  let service: ElectionOrganizerService
+  let seedOrganizer: ElectionOrganizer
+  let organizers: ElectionOrganizer[] = []
+
+  const passwordPassedIn = '@passwordIsSecret1099'
+  const rand = Math.random() * 10
+  beforeAll(async () => {
+    db = await getTestDatabase()
+    service = new ElectionOrganizerService(db)
+    seedOrganizer = await (service.createAndSaveElectionOrganizer({
+      firstName: 'First organizer',
+      lastName: 'Last name org',
+      email: rand + 'testing@gmail.com',
+      password: passwordPassedIn
+    }) as Promise<ElectionOrganizer>)
+
+    organizers.push(seedOrganizer)
+  })
+
+  afterAll(async () => {
+    for (const organizer of organizers) {
+      console.log(organizer)
+      const org = await service.delete(organizer.id)
+    }
+    await db.close()
+  })
+
+  it('should create a election organizer', async () => {
+    expect(seedOrganizer).toBeInstanceOf(ElectionOrganizer)
+  })
+
+  it('should generate a hash for the password', async () => {
+    expect(seedOrganizer.password).not.toBe(passwordPassedIn)
+  })
+})

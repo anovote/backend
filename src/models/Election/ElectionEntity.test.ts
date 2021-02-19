195cb534fbbebcd3983c7e663c2bcb0e283bf921
Steffen Holanger <holanger.steffen@gmail.com>
2021-02-19 14:51:22 +0100

    clean file

--- src/models/Election/ElectionEntity.test.ts
+++ src/models/Election/ElectionEntity.test.ts
@@ -1,25 +1,23 @@
-import { createConnection, getConnection, Repository } from 'typeorm'
+import { Connection, Repository } from 'typeorm'
 import { ElectionOrganizer } from '../ElectionOrganizer/ElectionOrganizerEntity'
 import { Election } from './ElectionEntity'
 import { ElectionStatus } from './ElectionStatus'
-import config from '@/config'
 import setupConnection from '@/loaders/setupTestDB'
 
 let repo: Repository<Election>
+let conn: Connection
 
 beforeEach(async () => {
-  await setupConnection()
-  repo = getConnection('test').getRepository(Election)
+  conn = await setupConnection()
+  repo = conn.getRepository(Election)
 })
 
 afterEach(() => {
-  let conn = getConnection('test')
   return conn.close()
 })
 
 test('Election without status set should return entity with status set to default', async () => {
   const election = repo.create()
-  //   const election = new Election()
   election.title = 'My Test'
   election.description = 'This is a dummy'
   election.isAutomatic = false
@@ -34,9 +32,10 @@ test('Election without status set should return entity with status set to defaul
     }
   })
 
-  expect(el[0].status).toBe<ElectionStatus>(ElectionStatus.NotStarted)
-  expect(el[0].status).not.toBe<ElectionStatus>(ElectionStatus.Started)
-  expect(el[0].status).not.toBe<ElectionStatus>(ElectionStatus.Finished)
+  let firstElection = el[0]
+  expect(firstElection.status).toBe<ElectionStatus>(ElectionStatus.NotStarted)
+  expect(firstElection.status).not.toBe<ElectionStatus>(ElectionStatus.Started)
+  expect(firstElection.status).not.toBe<ElectionStatus>(ElectionStatus.Finished)
 })
 
 test('Election with status set to Started should return with status started', async () => {
@@ -56,7 +55,8 @@ test('Election with status set to Started should return with status started', as
     }
   })
 
-  expect(el[0].status).toBe<ElectionStatus>(ElectionStatus.Started)
-  expect(el[0].status).not.toBe<ElectionStatus>(ElectionStatus.NotStarted)
-  expect(el[0].status).not.toBe<ElectionStatus>(ElectionStatus.Finished)
+  let firstElection = el[0]
+  expect(firstElection.status).toBe<ElectionStatus>(ElectionStatus.Started)
+  expect(firstElection.status).not.toBe<ElectionStatus>(ElectionStatus.NotStarted)
+  expect(firstElection.status).not.toBe<ElectionStatus>(ElectionStatus.Finished)
 })

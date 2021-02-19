89b8f558a3d13977dae9a1a4839e13c92df5063e
Steffen Holanger <holanger.steffen@gmail.com>
2021-02-19 17:59:46 +0100

    fix tests

---
+++ src/models/Ballot/BallotEntity.test.ts
@@ -0,0 +1,56 @@
+import { createConnection, getConnection, Repository } from 'typeorm'
+import setupConnection from '@/loaders/setupTestDB'
+import { Ballot } from './BallotEntity'
+import { BallotResultDisplay } from './BallotResultDisplay'
+import { Election } from '../Election/ElectionEntity'
+import config from '@/config'
+
+let repo: Repository<Ballot>
+
+beforeAll(async () => {
+  await setupConnection()
+  repo = getConnection('test').getRepository(Ballot)
+})
+
+beforeEach(async () => {
+  repo = getConnection('test').getRepository(Ballot)
+  const allEntries = await repo.find()
+  repo.remove(allEntries)
+})
+
+afterAll(() => {
+  let conn = getConnection('test')
+  return conn.close()
+})
+
+test('Ballot without result display type set should return entity with display type set to default', async () => {
+  //   await setupConnection()
+  repo = getConnection('test').getRepository(Ballot)
+  const ballot = repo.create()
+  ballot.title = 'My Test'
+  ballot.description = 'This is a dummy'
+  ballot.election = new Election()
+  ballot.order = 1
+  await repo.save(ballot)
+  const ballots = await repo.find()
+  const firstBallot = ballots[0]
+  expect(firstBallot.resultDisplayType).toBe<BallotResultDisplay>(BallotResultDisplay.SINGLE)
+  expect(firstBallot.resultDisplayType).not.toBe<BallotResultDisplay>(BallotResultDisplay.NONE)
+  expect(firstBallot.resultDisplayType).not.toBe<BallotResultDisplay>(BallotResultDisplay.ALL)
+  expect(firstBallot.resultDisplayType).not.toBe<BallotResultDisplay>(BallotResultDisplay.RUNNER_UP)
+})
+
+test('Ballot with display type set to Runner up should return with status runner up', async () => {
+  const ballot = repo.create()
+  ballot.title = 'My Test'
+  ballot.description = 'This is a dummy'
+  ballot.resultDisplayType = BallotResultDisplay.RUNNER_UP
+  ballot.order = 1
+  await repo.save(ballot)
+  const ballots = await repo.find()
+  const firstBallot = ballots[0]
+  expect(firstBallot.resultDisplayType).toBe<BallotResultDisplay>(BallotResultDisplay.RUNNER_UP)
+  expect(firstBallot.resultDisplayType).not.toBe<BallotResultDisplay>(BallotResultDisplay.NONE)
+  expect(firstBallot.resultDisplayType).not.toBe<BallotResultDisplay>(BallotResultDisplay.ALL)
+  expect(firstBallot.resultDisplayType).not.toBe<BallotResultDisplay>(BallotResultDisplay.SINGLE)
+})
--- src/models/Election/ElectionEntity.test.ts
+++ src/models/Election/ElectionEntity.test.ts
@@ -1,4 +1,4 @@
-import { Connection, Repository } from 'typeorm'
+import { Connection, getConnection, Repository } from 'typeorm'
 import { ElectionOrganizer } from '../ElectionOrganizer/ElectionOrganizerEntity'
 import { Election } from './ElectionEntity'
 import { ElectionStatus } from './ElectionStatus'
@@ -7,16 +7,37 @@ import setupConnection from '@/loaders/setupTestDB'
 let repo: Repository<Election>
 let conn: Connection
 
-beforeEach(async () => {
-  conn = await setupConnection()
-  repo = conn.getRepository(Election)
+beforeAll(async () => {
+  try {
+    conn = await setupConnection()
+    console.log('waiting for connection')
+
+    conn ? console.log('Connected') : console.log('not connected')
+    return conn
+  } catch (err) {
+    console.log(err)
+  }
 })
 
-afterEach(() => {
-  return conn.close()
+beforeEach(async () => {
+  repo = getConnection('test').getRepository(Election)
+
+  const entries = await repo.find()
+  repo.remove(entries)
+})
+
+afterAll(async () => {
+  try {
+    console.log('disconnecting')
+
+    return await conn.close()
+  } catch (err) {
+    console.log(err)
+  }
 })
 
 test('Election without status set should return entity with status set to default', async () => {
+  repo = conn.getRepository(Election)
   const election = repo.create()
   election.title = 'My Test'
   election.description = 'This is a dummy'
@@ -24,14 +45,12 @@ test('Election without status set should return entity with status set to defaul
   election.isLocked = false
   election.electionOrganizer = new ElectionOrganizer()
   election.eligibleVoters = []
-
   await repo.save(election)
   const el = await repo.find({
     where: {
       id: 1
     }
   })
-
   let firstElection = el[0]
   expect(firstElection.status).toBe<ElectionStatus>(ElectionStatus.NotStarted)
   expect(firstElection.status).not.toBe<ElectionStatus>(ElectionStatus.Started)
@@ -39,6 +58,7 @@ test('Election without status set should return entity with status set to defaul
 })
 
 test('Election with status set to Started should return with status started', async () => {
+  repo = conn.getRepository(Election)
   const election = repo.create()
   election.title = 'My Test'
   election.description = 'This is a dummy'
@@ -46,15 +66,9 @@ test('Election with status set to Started should return with status started', as
   election.isLocked = false
   election.electionOrganizer = new ElectionOrganizer()
   election.eligibleVoters = []
-
   election.status = ElectionStatus.Started
   await repo.save(election)
-  const el = await repo.find({
-    where: {
-      id: 1
-    }
-  })
-
+  const el = await repo.find()
   let firstElection = el[0]
   expect(firstElection.status).toBe<ElectionStatus>(ElectionStatus.Started)
   expect(firstElection.status).not.toBe<ElectionStatus>(ElectionStatus.NotStarted)

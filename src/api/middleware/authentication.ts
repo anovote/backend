36238c8ae758ba90236441903613c1a5fa497614
Steffen Holanger <44159536+spiftire@users.noreply.github.com>
2021-02-24 08:39:47 +0100

    fix missing db

--- src/api/middleware/authentication.ts
+++ src/api/middleware/authentication.ts
@@ -3,6 +3,7 @@ import { ElectionOrganizer } from '@/models/ElectionOrganizer/ElectionOrganizerE
 import { AuthenticationService } from '@/services/AuthenticationService'
 import { ElectionOrganizerService } from '@/services/ElectionOrganizerService'
 import { NextFunction, Request, Response } from 'express'
+import { getConnection } from 'typeorm'
 /**
  * Verifies if a token is passed along with the request.
  * Checks if the request have the rights to access the route.
@@ -11,8 +12,9 @@ import { NextFunction, Request, Response } from 'express'
  * @param next call next middleware/route
  */
 export const checkAuth = async (req: Request, res: Response, next: NextFunction) => {
+  const db = getConnection()
   const authenticationService = new AuthenticationService()
-  const electionOrganizerService = new ElectionOrganizerService()
+  const electionOrganizerService = new ElectionOrganizerService(db)
   try {
     const id = (await authenticationService.verifyToken(req.headers.authorization)).id
     const electionOrganizer = await electionOrganizerService.getElectionOrganizerById(id)

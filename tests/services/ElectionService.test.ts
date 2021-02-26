4fb315f8699d53dceb4345397c9e1c941fc08de1
Steffen Holanger <44159536+spiftire@users.noreply.github.com>
2021-02-26 10:52:42 +0100

    fix tests

--- tests/services/ElectionService.test.ts
+++ tests/services/ElectionService.test.ts
@@ -8,19 +8,19 @@ import { ElectionOrganizer } from '@/models/ElectionOrganizer/ElectionOrganizerE
 import { ElectionService } from '@/services/ElectionService'
 import { Connection } from 'typeorm'
 import { createDummyOrganizer, deleteDummyOrganizer } from '../helpers/seed/organizer'
+import { clearDatabaseEntityTable } from '../Tests.utils'
 
 let db: Connection
 let organizer: ElectionOrganizer
 let electionService: ElectionService
-let elections: Election[] = []
 let seedElection: Election
+let seedDTO: IElection
 
 beforeAll(async () => {
   db = await setupConnection()
   organizer = await createDummyOrganizer(db)
-  electionService = new ElectionService(db, organizer)
 
-  seedElection = await (electionService.createElection({
+  seedDTO = {
     electionOrganizer: organizer,
     title: 'Some title',
     description: 'Some long description',
@@ -28,15 +28,18 @@ beforeAll(async () => {
     status: ElectionStatus.NotStarted,
     isLocked: true,
     isAutomatic: false
-  }) as Promise<Election>)!
+  }
+  electionService = new ElectionService(db, organizer)
+})
+
+beforeEach(async () => {
+  electionService = new ElectionService(db, organizer)
+  const repo = db.getRepository(Election)
+  await clearDatabaseEntityTable(repo)
 })
 
 afterAll(async () => {
   try {
-    for (const election of elections) {
-      await electionService.deleteElectionById(election.id)
-    }
-    await electionService.deleteElectionById(seedElection.id)
     await deleteDummyOrganizer(db, organizer)
     await db.close()
   } catch (err) {
@@ -57,7 +60,6 @@ it('should create a election with all data filled out', async () => {
     isAutomatic: false
   })
   expect(election).toBeInstanceOf(Election)
-  elections.push(election!)
 })
 
 it('should create a election without image, openDate and closeDate', async () => {
@@ -70,42 +72,47 @@ it('should create a election without image, openDate and closeDate', async () =>
     isAutomatic: false
   })
   expect(election).toBeInstanceOf(Election)
-  elections.push(election!)
 })
 
 it('should throw error when status is out of range', async () => {
+  seedElection = (await electionService.create(seedDTO)) as Election
   const election = deepCopy<IElection>(seedElection)
   election.status = 99 // Out of range of ENUM
   try {
-    await electionService.createElection(election)
+    expect(electionService.createElection(election)).rejects.toThrowError()
   } catch (error) {
-    expect(error).toBeInstanceOf(Error)
+    console.log(error)
   }
 })
 
 it('should not update if no election exists', async () => {
   try {
-    const updated = await electionService.updateElectionById(100, deepCopy<IElection>(seedElection))
-  } catch (e) {
-    expect(e).toBeInstanceOf(NotFoundError)
+    await expect(electionService.updateElectionById(100, deepCopy<IElection>(seedElection))).rejects.toThrowError(
+      NotFoundError
+    )
+  } catch (error) {
+    console.log(error)
   }
 })
 
 it('should not change id of updated election', async () => {
-  const passedInId = seedElection.id
-  const data = deepCopy<Election>(seedElection)
-  data.id = 5
-  const election = await electionService.updateElectionById(passedInId, deepCopy<IElection>(data))
-  expect(election!.id).toBe(passedInId)
+  const election = (await electionService.create(seedDTO)) as Election
+  const passedInId = election.id
+  const electionCopy = deepCopy<Election>(election)
+
+  electionCopy.id = 5
+  const updatedElection = await electionService.updateElectionById(passedInId, deepCopy<IElection>(electionCopy))
+  expect(updatedElection!.id).toBe(passedInId)
 })
 
 it('should return the election with given id', async () => {
-  const election = await electionService.getElectionById(seedElection.id)
+  seedElection = (await electionService.create(seedDTO)) as Election
+  const election = await electionService.getById(seedElection.id)
   expect(election!.id).toBe(seedElection.id)
 })
 
 it('should return undefined if election does not exist', async () => {
-  const election = await electionService.getElectionById(999999)
+  const election = await electionService.getById(999999)
   expect(election).toBeUndefined()
 })
 
@@ -123,8 +130,8 @@ it('should delete a election which exists', async () => {
 
 it('should throw not found error when deleting a election which do not exist', async () => {
   try {
-    const deleted = await electionService.deleteElectionById(99999999)
+    await expect(electionService.deleteElectionById(99999999)).rejects.toThrowError(NotFoundError)
   } catch (error) {
-    expect(error).toBeInstanceOf(NotFoundError)
+    console.log(error)
   }
 })

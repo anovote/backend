34835e5d7a948f5be494bbc2577ff4d3ef875f44
Steffen Holanger <44159536+spiftire@users.noreply.github.com>
2021-03-04 13:12:03 +0100

    add init service

---
+++ src/lib/errors/websocket/WebSocketService.ts
@@ -0,0 +1,63 @@
+import { SocketRoomEntity, SocketRoomState } from '@/models/SocketRoom/SocketRoomEntity'
+import { SocketRoomService } from '@/services/SocketRoomService'
+import { Socket } from 'socket.io'
+import { ForbiddenError } from '../http/ForbiddenError'
+
+/**
+ * Web socket service is responsible for opening and closing rooms, keeping an array of opened rooms
+ */
+export class WebSocketService {
+    // openRooms: SocketRoomEntity[]
+    socketConnection: Socket
+    socketRoomService: SocketRoomService
+
+    constructor(socketConnection: Socket, socketRoomService: SocketRoomService) {
+        this.socketConnection = socketConnection
+        this.socketRoomService = socketRoomService
+    }
+
+    closeRoom(room: SocketRoomEntity) {
+        this.socketConnection.removeAllListeners(room.id.toString())
+        room.roomState = SocketRoomState.CLOSE
+    }
+
+    openRoom(room: SocketRoomEntity) {
+        // todo open room
+        room.roomState = SocketRoomState.OPEN
+    }
+
+    openRooms(rooms: SocketRoomEntity[]) {
+        for (const room of rooms) {
+            this.openRoom(room)
+        }
+    }
+
+    async addUserToRoom(token: VoterToken, clientSocket: Socket) {
+        const room = await this.socketRoomService.getById(token.electionId)
+
+        if (!room) {
+            throw new ForbiddenError()
+        }
+
+        if (room.roomState === SocketRoomState.CLOSE) {
+            throw new RoomNotOpenError(`The room with id ${token.electionId} is not open`)
+        }
+
+        const electionIdStr = token.electionId.toString()
+        await clientSocket.join(electionIdStr)
+        return clientSocket
+    }
+}
+
+interface VoterToken {
+    electionId: number
+    voterId: number
+}
+
+class RoomNotOpenError extends Error {
+    constructor(message?: string) {
+        super(message)
+    }
+}
+
+// todo add room to election on create. update room status. write tests

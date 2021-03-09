caa999e4dc3c19fe690cb1cdbb57557d6cb4e7d8
Emil <emil.nilsen9888@gmail.com>
2021-03-09 13:28:35 +0100

    Remove unused method

--- src/helpers/array.ts
+++ src/helpers/array.ts
@@ -1,13 +1,3 @@
-export function trimItemsInStringArray(array: string[]): string[] {
-    const trimmedList: string[] = []
-
-    for (let i = 0; i < array.length; i++) {
-        trimmedList.push(array[i].trim())
-    }
-
-    return trimmedList
-}
-
 export function filterForDuplicates<T>(array: T[]): T[] {
     return array.filter(function (elem, index, self) {
         return index === self.indexOf(elem)

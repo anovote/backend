fff3b54bbd0c8fe24e00f81dde8cdd18f96761be
Sander Hurlen <sander.hurlen@icloud.com>
2021-03-10 18:48:33 +0100

    refactor to start route/auth

--- src/api/routes/auth/index.ts
+++ src/api/routes/auth/index.ts
@@ -1,13 +1,13 @@
+import config from '@/config'
 import { BadRequestError } from '@/lib/errors/http/BadRequestError'
 import { database } from '@/loaders'
 import mailTransporter from '@/loaders/nodemailer'
 import { AuthenticationService } from '@/services/AuthenticationService'
 import { ElectionOrganizerService } from '@/services/ElectionOrganizerService'
-import { ElectionService } from '@/services/ElectionService'
 import { EligibleVoterService } from '@/services/EligibleVoterService'
 import { EncryptionService } from '@/services/EncryptionService'
 import { MailService } from '@/services/MailService'
-import { VerificationService } from '@/services/VerificationService'
+import { VoterVerificationService } from '@/services/VoterVerificationService'
 import { Router } from 'express'
 import { StatusCodes } from 'http-status-codes'
 
@@ -32,45 +32,6 @@ router.post('/login', async (request, response, next) => {
         response.json({ token })
     } catch (error) {
         next(error)
-    }
-})
-
-router.get('/join', async (request, response, next) => {
-    try {
-        const verificationService = new VerificationService(
-            new MailService(await mailTransporter()),
-            new EncryptionService()
-        )
-        const eligibleVoterService = new EligibleVoterService(database)
-        const voter = await eligibleVoterService.getVoterByIdentification('sanderhur1@gmail.com') // TODO replace with actual
-
-        const hashed = await new EncryptionService().hash(eligibleVoterService.generateVerificationString(voter))
-        const readyVoter = await eligibleVoterService.storeVerificationHash(voter.identification, hashed)
-
-        const owner = await new ElectionOrganizerService(database).getById(1)
-        const election = await new ElectionService(database, owner!).getById(1) // TODO replace with actual
-
-        await verificationService.stage(readyVoter!, election!)
-        response.json('Check your mail')
-    } catch (err) {
-        next(err)
-    }
-})
-
-router.get('/verify', async (request, response, next) => {
-    const { v } = request.query
-    if (v) {
-        const verification = v?.toString()
-        try {
-            const eligibleVoterService = new EligibleVoterService(database)
-            const voter = await eligibleVoterService.getVerificationHash(verification!)
-            await eligibleVoterService.markAsVerified(voter)
-            response.json('Successfully verified')
-        } catch (err) {
-            next(err)
-        }
-    } else {
-        response.json('Please try again later')
     }
 })
 

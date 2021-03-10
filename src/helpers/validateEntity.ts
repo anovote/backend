e2d56db2e2baadba81d0118f655170d3833b36ce
Emil <emil.nilsen9888@gmail.com>
2021-03-10 15:40:06 +0100

    Remove console log

--- src/helpers/validateEntity.ts
+++ src/helpers/validateEntity.ts
@@ -10,8 +10,6 @@ export const validateEntity = async (entity: object, validatorOptions?: Validato
     const isValid = validation.length === 0
 
     if (!isValid) {
-        console.log(validation)
-
         const name = validation[0].target?.constructor.name
         throw new ValidationError({ message: `Validation failed for ${name}`, validation: validation })
     }

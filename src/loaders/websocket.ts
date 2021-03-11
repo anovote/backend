5d4fc4b024d23bf2d78f0aca705d5233cf73e0a9
Emil <emil.nilsen9888@gmail.com>
2021-03-11 15:55:31 +0100

    Add socket functionality

--- src/loaders/websocket.ts
+++ src/loaders/websocket.ts
@@ -1,10 +1,21 @@
+import { createDummyBallot } from '@/../tests/helpers/seed/ballot'
+import { createDummyCandidate } from '@/../tests/helpers/seed/candidate'
+import { createDummyElection } from '@/../tests/helpers/seed/election'
+import { createDummyOrganizer } from '@/../tests/helpers/seed/organizer'
 import { AnoSocket } from '@/lib/errors/websocket/AnoSocket'
 import { validateConnection } from '@/lib/errors/websocket/middleware/ValidateConnection'
+import { Ballot } from '@/models/Ballot/BallotEntity'
+import { Candidate } from '@/models/Candidate/CandidateEntity'
+import { Election } from '@/models/Election/ElectionEntity'
+import { ElectionOrganizer } from '@/models/ElectionOrganizer/ElectionOrganizerEntity'
+import { IVote } from '@/models/Vote/IVote'
 import { SocketRoomService } from '@/services/SocketRoomService'
+import { VoteService } from '@/services/VoteService'
 import chalk from 'chalk'
 import { Application } from 'express'
 import http from 'http'
 import { Server } from 'socket.io'
+import { database } from '.'
 import { logger } from './logger'
 
 export default (expressApp: Application) => {
@@ -30,7 +41,7 @@ export default (expressApp: Application) => {
         const socketId = chalk.blue(socketConnection.id)
         logger.info(`${socketId} was connected`)
 
-        await socketRoomService.addUserToRoom(socketConnection, socketServer)
+        //await socketRoomService.addUserToRoom(socketConnection, socketServer)
 
         // if (socketConnection.token.organizer) {
         //     // TODO Add organizer events
@@ -47,6 +58,23 @@ export default (expressApp: Application) => {
 
         socketConnection.on('disconnect', (reason) => {
             logger.info(`${socketId} was disconnected due to: ${reason}`)
+        })
+
+        socketConnection.on('vote_submitted', async (vote: IVote) => {
+            const organizer: ElectionOrganizer = await createDummyOrganizer(database)
+            const election: Election = await createDummyElection(database, organizer)
+            const ballot: Ballot = await createDummyBallot(database, election)
+            const candidate: Candidate = await createDummyCandidate(database, ballot)
+            const voteService: VoteService = new VoteService(database)
+
+            vote = {
+                candidate: candidate.id,
+                voterId: 696969,
+                submitted: new Date(),
+                ballotId: ballot.id
+            }
+
+            await voteService.create(vote)
         })
     })
 

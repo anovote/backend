7d0ff334f4f72078a284b330f28780f73525ce00
Sander Hurlen <sander.hurlen@icloud.com>
2021-03-11 12:53:10 +0100

    add documentation for voter verification and return socket id

--- src/services/VoterVerificationService.ts
+++ src/services/VoterVerificationService.ts
@@ -19,35 +19,67 @@ export class VoterVerificationService {
         this._eligibleVoterService = eligibleVoterService
     }
 
+    /**
+     * Stages a voter for verification to an election by sending out a mail for the voter to verify with
+     * The mail consists of a encrypted code with unique identifiers for the voter to be able to
+     * prove its identity.
+     * @param voter the eligible voter to stage for an election
+     * @param forElection the election the voter attempts to join
+     * @param socket the socket identification the encrypted code should contain
+     */
     async stage(voter: EligibleVoter, forElection: IElection, socket: string) {
         const encryptedCode = this._encryptionService.encrypt(this.generateVerificationCode(voter, socket))
         await this._mailer.sendVerificationMail(voter.identification, encryptedCode, forElection)
     }
 
+    /**
+     * Verifies the authenticity of an staged voter.
+     * The verification provided are able to prove the voters unique identification
+     * @param verification the encrypted string to verify the identity of the voter with
+     * @returns the socket id from the original connection for the voter
+     */
     async verify(verification: string) {
         const decrypted = this._encryptionService.decrypt(verification)
 
         const id = this.getIdFromVerificationCode(decrypted)
+        const socketId = this.getSocketIdFromVerificationCode(decrypted)
 
         const voter = await this._eligibleVoterService.getById(Number.parseInt(id))
+        await this._eligibleVoterService.markAsVerified(voter!)
 
-        return await this._eligibleVoterService.markAsVerified(voter!)
+        return socketId
     }
 
-    // TODO can be moved to own class. Works fine for now
+    // TODO verification code functions should be handled in own class. Works fine for now
 
-    getIdFromVerificationCode(decrypted: string) {
+    /**
+     * Extracts the id from the unique verification code
+     * @param decrypted the decrypted string to get the id from
+     * @returns id, from the decrypted string
+     */
+    private getIdFromVerificationCode(decrypted: string) {
         const idIndex = 1
 
         return decrypted.split('_')[idIndex]
     }
 
-    getSocketIdFromVerificationCode(decrypted: string) {
+    /**
+     * Extracts the socket id from the unique verification code
+     * @param decrypted the decrypted string to get the id from
+     * @returns socket id, from the decrypted string
+     */
+    private getSocketIdFromVerificationCode(decrypted: string) {
         const socketIdIndex = 2
         return decrypted.split('_')[socketIdIndex]
     }
 
-    generateVerificationCode(voter: EligibleVoter, socketId: string) {
+    /**
+     * Generates an unique string to be encrypted and sent over to the client to verify with.
+     * @param voter the eligible voter
+     * @param socketId the socket id of the voters original connection
+     * @returns a unique identification string
+     */
+    private generateVerificationCode(voter: EligibleVoter, socketId: string) {
         const identificationForMail = voter.identification.split('@')[0]
 
         return `${identificationForMail}_${voter.id}_${socketId}`

6026cee50e223cd8c0b3797544caefa58c3fb385
Christoffer A Traeen <christoffer.andersen@live.com>
2021-03-18 23:56:44 +0100

    Add todo

--- src/lib/events/client/submitVote.ts
+++ src/lib/events/client/submitVote.ts
@@ -14,23 +14,35 @@ export const submitVote: EventHandlerAcknowledges<IVote> = async (vote, socket, 
     const submittedVote: IVote = vote
     const voteService = new VoteService(database)
 
+    // Todo: send vote to organizer of the room
+    // Todo: verify if vote is valid
+    // Todo: verify that a voter has not voted already for the ballot
+    // Todo: verify that the ballot is the current
+    // Todo: send error if ballot does not exist
+    // Todo: send error if ballot is not current (not sent, or ended)
+    // Todo: send error if candidate is not existing on ballot
+    // Todo: verify that a vote has not more candidates than allowed
+
     if (!submittedVote.candidate || !submittedVote.ballot || !submittedVote.voter) {
         acknowledgement({
             statusCode: StatusCodes.BAD_REQUEST,
             message: 'Please provide the required data'
-        })
+            // Temp fix
+        } as any)
     } else {
         try {
             await voteService.create(submittedVote)
             acknowledgement({
                 statusCode: StatusCodes.OK,
                 message: 'Vote was submitted!'
-            })
+                // Temp fix
+            } as any)
         } catch (err) {
             acknowledgement({
                 statusCode: StatusCodes.FORBIDDEN,
                 message: 'We were not able to submit your vote'
-            })
+                // Temp fix
+            } as any)
         }
     }
 }

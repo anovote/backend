da3287eec8021dbe0647c32370944ff3f944a4f5
Christoffer A Traeen <christoffer.andersen@live.com>
2021-03-30 12:17:32 +0200

    Add joined

---
+++ src/lib/websocket/events/client/voter/joined.ts
@@ -0,0 +1,16 @@
+import { ElectionCode } from '@/lib/voting/ElectionCode'
+import { VoterId } from '@/lib/voting/VoterId'
+import { VoterSocket } from '@/lib/websocket/AnoSocket'
+import { EventHandler } from '@/lib/websocket/EventHandler'
+import { SocketRoomService } from '@/services/SocketRoomService'
+
+/**
+ * Assigns electionCode, voter ID to the joined voter socket and adds the voter to the appropriate room
+ * @param event event details
+ */
+export const joined: EventHandler<{ electionCode: ElectionCode; voterId: VoterId }> = async (event) => {
+    const voterSocket = event.client as VoterSocket
+    voterSocket.electionCode = event.data.electionCode
+    voterSocket.voterId = event.data.voterId
+    await SocketRoomService.getInstance().addUserToRoom(voterSocket, event.server)
+}

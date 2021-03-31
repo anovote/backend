106f2e176a5a46b79e555988a376de2a3b768f3e
Christoffer A Traeen <christoffer.andersen@live.com>
2021-03-31 16:47:34 +0200

    Fix join election enpoint

--- src/api/routes/voter/joinElection.ts
+++ src/api/routes/voter/joinElection.ts
@@ -1,13 +1,23 @@
-import { database } from '@/loaders'
+import { NotFoundError } from '@/lib/errors/http/NotFoundError'
+import { ServerErrorMessage } from '@/lib/errors/messages/ServerErrorMessages'
 import { SocketRoomService } from '@/services/SocketRoomService'
 import { Router } from 'express'
 
 const router = Router()
 
 router.get('/:electionId', async (request, response, next) => {
-    const electionId = Number.parseInt(request.params.electionId)
-    const socketRoomService = SocketRoomService.getInstance()
-    await socketRoomService.getById(electionId)
+    try {
+        const electionId = Number.parseInt(request.params.electionId)
+        const socketRoomService = SocketRoomService.getInstance()
+        const socketRoom = await socketRoomService.getById(electionId)
+        if (!socketRoom) {
+            throw new NotFoundError({ message: ServerErrorMessage.notFound('Room') })
+        } else {
+            response.send(socketRoom)
+        }
+    } catch (error) {
+        next(error)
+    }
 })
 
 export default router

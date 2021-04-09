a5343c4754b2b077d1376925c9503ced34fd6939
Steffen Holanger <44159536+spiftire@users.noreply.github.com>
2021-04-09 11:46:12 +0200

    add ElectionStatService

---
+++ src/services/ElectionStats.ts
@@ -0,0 +1,56 @@
+import { BallotVoteStats } from '@/lib/voting/BallotStats'
+import { logger } from '@/loaders/logger'
+import { Ballot } from '@/models/Ballot/BallotEntity'
+import { ElectionOrganizer } from '@/models/ElectionOrganizer/ElectionOrganizerEntity'
+import { Vote } from '@/models/Vote/VoteEntity'
+import { Connection } from 'typeorm'
+import { ElectionService } from './ElectionService'
+import { VoteService } from './VoteService'
+
+export class ElectionStatsService {
+    database: Connection
+    owner: ElectionOrganizer
+
+    constructor(database: Connection, owner: ElectionOrganizer) {
+        this.database = database
+        this.owner = owner
+    }
+    async getElectionStats(electionId: number) {
+        const election = await new ElectionService(this.database, this.owner).getById(electionId)
+        if (!election) {
+            throw new Error('No election')
+        }
+        const { ballots } = election
+
+        const stats: IBallotStats[] = []
+
+        const voteService = new VoteService(this.database)
+
+        for await (const ballot of ballots) {
+            const ballotVotes = await voteService.getByBallotId(ballot.id)
+            stats.push(this.summarizeVotes(ballotVotes, ballot))
+        }
+        return stats
+    }
+
+    private summarizeVotes(votes: Vote[], ballot: Ballot) {
+        const stats: BallotVoteStats = new BallotVoteStats(ballot)
+        stats.addVotes(votes)
+        return stats.getStats()
+    }
+}
+
+interface IBallotStats {
+    ballotId: number
+    stats: {
+        total: number
+        votes: number
+        blank: number
+        candidates: Array<ICandidateStats>
+    }
+}
+
+interface ICandidateStats {
+    id: number
+    votes: number
+}

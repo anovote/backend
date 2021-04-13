1e0a145a43f5620a0278feafa1073afd3d822020
Emil <emil.nilsen9888@gmail.com>
2021-04-13 13:26:08 +0200

    Update events

--- src/lib/websocket/events/client/voter/voterDisconnect.ts
+++ src/lib/websocket/events/client/voter/voterDisconnect.ts
@@ -1,9 +1,8 @@
 import { VoterSocket } from '@/lib/websocket/AnoSocket'
 import { EventHandler } from '@/lib/websocket/EventHandler'
 import { SocketRoomService } from '@/services/SocketRoomService'
-import { Server } from 'socket.io'
 
-export const voterDisconnect: EventHandler<{ client: VoterSocket; server: Server }> = (event) => {
+export const voterDisconnect: EventHandler<undefined> = (event) => {
     const voterSocket = event.client as VoterSocket
     SocketRoomService.getInstance().removeUserFromRoom(voterSocket, event.server)
 }

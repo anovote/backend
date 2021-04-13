c48987a75fd97fd1dfb77e388cbed5a21692258d
Emil <emil.nilsen9888@gmail.com>
2021-04-13 12:47:15 +0200

    Add eventhandler type

--- src/lib/websocket/events/client/voter/voterDisconnect.ts
+++ src/lib/websocket/events/client/voter/voterDisconnect.ts
@@ -1,7 +1,9 @@
 import { VoterSocket } from '@/lib/websocket/AnoSocket'
+import { EventHandler } from '@/lib/websocket/EventHandler'
 import { SocketRoomService } from '@/services/SocketRoomService'
 import { Server } from 'socket.io'
 
-export const voterDisconnect = (client: VoterSocket, server: Server) => {
-    SocketRoomService.getInstance().removeUserFromRoom(client, server)
+export const voterDisconnect: EventHandler<{ client: VoterSocket; server: Server }> = (event) => {
+    const voterSocket = event.client as VoterSocket
+    SocketRoomService.getInstance().removeUserFromRoom(voterSocket, event.server)
 }

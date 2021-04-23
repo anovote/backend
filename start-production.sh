3f4fa0500b93e1b54448ed18ea09a159f0f91503
Christoffer Andersen Træen <christoffer.andersen@live.com>
2021-04-23 16:13:16 +0000

    Update production scripts

--- start-production.sh
+++ start-production.sh
@@ -1,12 +1,35 @@
+#!/bin/bash
 printf " \n - STARTING ANOVOTE PRODUCTION SETUP\n\n"
 
+function taskDone()
+{
+    printf " \n----- DONE";
+}
+
+########################################################################
+
+printf " \n\n--- STOPING OLD SERVICES\n\n"
+
+./anovote prod --down
+
+taskDone
+########################################################################
+
 printf " \n\n--- SETINGUP HTTP FOLDER\n\n"
+
 mkdir -p ../http/nginx
 cp ./nginx-config/certbot.conf ../http/nginx
+
+taskDone
+########################################################################
 
 printf " \n\n--- SETTING UP CERTIFICATES\n\n"
 
 ./init-letsencrypt.sh
+./anovote prod --down
+
+taskDone
+########################################################################
 
 printf " \n\n--- FETCHING FRONTEND\n\n"
 
@@ -20,6 +43,9 @@ else
     cd frontend
 fi
 
+taskDone
+########################################################################
+
 printf " \n\n--- BUILDING FRONTEND\n\n"
 
 # FRONTEND BUILD
@@ -28,11 +54,18 @@ git checkout feature/add-docker-scripts
 # Go back from frontend path
 cd ../backend
 
-printf " \n\n--- STARTING BACKEND\n\n"
+taskDone
+########################################################################
 
 printf " \n\n--- MOVING PROXY CONFIG\n\n"
+
 cp ./nginx-config/proxy.conf ../http/nginx
 cp ./nginx-config/mime.types ../http/nginx
 
+taskDone
+########################################################################
+printf " \n\n--- STARTING BACKEND\n\n"
+
 # BACKEND BUILD
 ./anovote prod --build
+printf " \n\n----- PRODUCTION STARTED\n\n"

2878114558adc13a599b198cddb90413a68e0c25
Christoffer A Traeen <christoffer.andersen@live.com>
2021-04-29 19:59:39 +0200

    Refactor push ballot

--- src/lib/websocket/events/client/organizer/pushBallot.ts
+++ src/lib/websocket/events/client/organizer/pushBallot.ts
@@ -1,67 +1,97 @@
+import { BaseError } from '@/lib/errors/BaseError'
+import { NotFoundError } from '@/lib/errors/http/NotFoundError'
+import { ServerErrorMessage } from '@/lib/errors/messages/ServerErrorMessages'
 import { OrganizerSocket, VoterSocket } from '@/lib/websocket/AnoSocket'
 import { EventHandlerAcknowledges } from '@/lib/websocket/EventHandler'
 import { EventErrorMessage, EventMessage } from '@/lib/websocket/EventResponse'
 import { Events } from '@/lib/websocket/events'
 import { database } from '@/loaders'
 import { logger } from '@/loaders/logger'
+import { Ballot } from '@/models/Ballot/BallotEntity'
 import { BallotStatus } from '@/models/Ballot/BallotStatus'
 import { ElectionOrganizer } from '@/models/ElectionOrganizer/ElectionOrganizerEntity'
 import { BallotService } from '@/services/BallotService'
 import { ElectionService } from '@/services/ElectionService'
 import { SocketRoomService } from '@/services/SocketRoomService'
-import { Events } from '@/lib/websocket/events'
+import { Server } from 'socket.io'
 
+/**
+ * Emits the provided ballot to all voter sockets that has not
+ * voted yet.
+ * @param server the socket io server
+ * @param roomId the room to emit to
+ * @param ballotVoters all voters for the ballot
+ * @param ballot the ballot to emit
+ */
+function emitBallotToNotVotedSockets(server: Server, roomId: string, ballotVoters: Set<number>, ballot: Ballot) {
+    // Get all socket IDs for the socket room that the ballot belongs to
+    const roomSocketIds = server.of('/').adapter.rooms.get(roomId)
+    roomSocketIds?.forEach((socketId) => {
+        const socket = server.sockets.sockets.get(socketId) as VoterSocket | undefined
+        // If the voter socket exist and the voter id for that socket has not voted on the ballot yet
+        if (socket && !ballotVoters.has(socket.voterId)) {
+            // Emit the ballot only to those that has not voter on the ballot yet
+            socket.emit(Events.server.ballot.push, ballot)
+        }
+    })
+}
 /**
  * Responsible for pushing a ballot to all voters in a election.
  * @param event event data (socket, server, data, ack callback)
  */
 export const pushBallot: EventHandlerAcknowledges<{ ballotId: number; electionId: number }> = async (event) => {
-    const socketRoomService = SocketRoomService.getInstance()
-    const organizerSocket = event.client as OrganizerSocket
     try {
         const { electionId, ballotId } = event.data
-        // Add ballot to vote stats for the election room
+        const socketRoomService = SocketRoomService.getInstance()
+        const organizerSocket = event.client as OrganizerSocket
         const ballotService = new BallotService(database, new ElectionService(database), {
             id: organizerSocket.organizerId
         } as ElectionOrganizer)
-        const room = socketRoomService.getRoom(electionId)
+        const roomId = electionId.toString()
+        const roomSocket = event.server.to(roomId)
+        const room = socketRoomService.getRoom(roomId)
         const ballot = await ballotService.getById(ballotId)
-        if (room && ballot) {
-            // Update ballot to in progress
-            ballot.status = BallotStatus.IN_PROGRESS
-            await ballotService.update(ballot.id, ballot)
 
-            // Get voters for the ballot
-            const ballotVoters = room.getBallotVoters(ballotId)
-            if (ballotVoters) {
-                if (ballotVoters?.size == 0) {
-                    // Send to all as no one has voted on the ballot
-                    event.server.to(electionId.toString()).emit(Events.server.ballot.push, ballot)
-                } else {
-                    // Get all socket IDs for the socket room that the ballot belongs to
-                    const roomSocketIds = event.server.of('/').adapter.rooms.get(electionId.toString())
-                    roomSocketIds?.forEach((socketId) => {
-                        const socket = event.server.sockets.sockets.get(socketId) as VoterSocket | undefined
-                        // If the voter socket exist and the voter id for that socket has not voted on the ballot yet
-                        if (socket && ballotVoters && !ballotVoters.has(socket.voterId)) {
-                            // Emit the ballot only to those that has not voter on the ballot yet
-                            socket.emit(Events.server.ballot.push, ballot)
-                        }
-                    })
-                }
+        if (!room || !ballot) {
+            if (!room) {
+                throw new NotFoundError({
+                    message: ServerErrorMessage.notFound('Election room'),
+                    code: 'ELECTION_ROOM_NOT_EXIST'
+                })
             } else {
-                // Create ballot stats and voter set for the new ballot
-                room.createBallotVoteInformation(ballot)
-                // Broadcast to all since this ballot has not been created before now
-                event.server.to(electionId.toString()).emit(Events.server.ballot.push, ballot)
+                throw new NotFoundError({
+                    message: ServerErrorMessage.notFound('Ballot'),
+                    code: 'BALLOT_NOT_EXIST'
+                })
             }
-
-            // Return updated to organizer
-            event.acknowledgement(EventMessage({ ballot }))
         }
-        event.acknowledgement(EventMessage({}))
+
+        // If the ballot is archived, we do not want to proceed and just
+        // take an early exit
+        if (ballot.status === BallotStatus.IN_ARCHIVE) return
+
+        // Update ballot to in progress
+        ballot.status = BallotStatus.IN_PROGRESS
+        await ballotService.update(ballot.id, ballot)
+
+        // Create ballot vote information as we did not get any ballot voters
+        if (!room.ballotVoteInformationExists(ballot.id)) room.createBallotVoteInformation(ballot)
+
+        // Get voters for the ballot
+        const ballotVoters = room.getBallotVoters(ballotId)
+        if (ballotVoters.size == 0) {
+            // Send to all as no one has voted on the ballot
+            roomSocket.emit(Events.server.ballot.push, ballot)
+        } else {
+            emitBallotToNotVotedSockets(event.server, roomId, ballotVoters, ballot)
+        }
+
+        logger.info(`Ballot ${ballot.id} was pushed`)
+        // Return updated ballot to organizer
+        return event.acknowledgement(EventMessage({ ballot }))
     } catch (err) {
         logger.error(err)
-        event.acknowledgement(EventErrorMessage(err))
+        // Only emit errors that is safe to emit
+        if (err instanceof BaseError) event.acknowledgement(EventErrorMessage(err))
     }
 }

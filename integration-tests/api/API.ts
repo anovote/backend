5e95a85f6edf2f94bc8ddaa9a9973f2398c9d710
Christoffer A Traeen <christoffer.andersen@live.com>
2021-05-10 15:14:31 +0200

    Fix api url supertest

--- integration-tests/api/API.ts
+++ integration-tests/api/API.ts
@@ -1,5 +1,4 @@
 import supertest from 'supertest'
 import config from '../../src/config'
 
-// TODO: Fix URL
-export const request = supertest('http://127.0.0.1:' + config.http.port)
+export const request = supertest(`http://${config.http.host}:${config.http.port}`)
